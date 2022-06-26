2121c97045fdcf4d0efc6057195164ba714c2a66
chris <not@my.email>
2022-06-26 15:08:07 -0500

    default image added

--- A1Article/Article.jsx
+++ A1Article/Article.jsx
@@ -1,9 +1,12 @@
-import                  './Article.css';
-import React from       'react';
-import ArticleBar from  './ArticleBar.jsx';
-import _arc from        '../../arc/arc.js';
+import                          './Article.css';
+import React from               'react';
+import ArticleBar from          './ArticleBar.jsx';
+import _arc from                '../../arc/arc.js';
+import testImage from           './testImage.js';
+
 
 export default (props) => {
+  const image_not_found = '/images/medical-symbol.jpeg';
 
   // articles object is passed in from parent component
   const article = props.article;
@@ -11,11 +14,21 @@ export default (props) => {
   // create hash from the article title
   const hash = _arc.makeAnchorHash(article.title);
 
+  testImage(article.image, 'article_link_image');
+
   // return the article component
   return (
     <article id={hash} className='__article'>
       <a className="article_link" href={article.link} target="_blank">
-        <img className="article_link_image" src={article.image}></img>
+
+        <img
+          className="article_link_image"
+          src={article.image}
+          onError={(error) => {
+            error.currentTarget.src = image_not_found;
+          }}
+        />
+
       </a>
       <p className="article_heading">
         <a className="article_heading_link" href={article.link} target="_blank">{article.title}</a>
@@ -31,7 +44,9 @@ export default (props) => {
       </p>
 
       {/* article has component ArticleBar which contains the Update and Delete operations */}
+
       <ArticleBar article={article}/>
     </article>
   );
 };
+
